4b3065054e72523b81077d018e603a5ef22c27d6
Sumit Asthana <asthana.sumit23@gmail.com>
2017-09-05 20:43:05 +0530

    Fix version to 0.1.1

--- drafttopic/about.py
+++ drafttopic/about.py
@@ -1,5 +1,5 @@
 __name__ = "drafttopic"
-__version__ = "0.4.2"
+__version__ = "0.1.1"
 __author__ = ["Aaron Halfaker", "Sumit Asthana"]
 __author_email__ = ["ahalfaker@wikimedia.org", "asthana.sumit23@gmail.com"]
 __description__ = "A library for automatic detection of topics of new drafts on Wikipedia" + \
