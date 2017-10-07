616c534724ba61c8007fda228b09cf3cb723b143
Sumit Asthana <asthana.sumit23@gmail.com>
2017-10-08 01:37:50 +0530

    Add Wikiproject name to directory mapping
    See - https://phabricator.wikimedia.org/T172325

---
+++ drafttopic/utilities/tests/test_wikiprojects_common.py
@@ -0,0 +1,15 @@
+from nose.tools import eq_
+from ..wikiprojects_common import wptemplate2directory
+from ..wikiprojects_common import fetch_section_text
+import logging
+import json
+
+logger = logging.getLogger(__name__)
+
+def test_wptemplate2directory():
+    wp_name = 'Wikipedia:WikiProject The Apprentice UK'
+    wikiprojects_file = 'culture_parsed.json'
+    actual_path = ['Culture and the arts', 'Broadcasting', 'Television', 'Reality TV']
+    wps = json.loads(fetch_section_text(wikiprojects_file, logger))
+    path = wptemplate2directory(wp_name, wps)
+    eq_(path, actual_path)    
