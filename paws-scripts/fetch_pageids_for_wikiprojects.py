bf1b375cbac6754104329005dfd2986fcdc00d7d
Sumit Asthana <asthana.sumit23@gmail.com>
2017-11-21 02:46:48 +0530

    Prepend 'talk_' to headers and fix flake8

--- paws-scripts/fetch_pageids_for_wikiprojects.py
+++ paws-scripts/fetch_pageids_for_wikiprojects.py
@@ -1,8 +1,6 @@
 # coding: utf-8
 
-from datetime import datetime
 import os
-import pickle
 import pymysql
 import json
 import pandas as pd
@@ -15,8 +13,8 @@ def connect_to_db():
     conn = pymysql.connect(
         host=os.environ['MYSQL_HOST'],
         user=os.environ['MYSQL_USERNAME'],
-        password=os.environ['MYSQL_PASSWORD'], #don't print me!
-        
+        password=os.environ['MYSQL_PASSWORD'],  # don't print me!
+
         database='enwiki_p',
         charset='utf8',
         use_unicode=True
@@ -27,7 +25,7 @@ def connect_to_db():
 conn = connect_to_db()
 
 
-f = open('mid-level-wp.json','r')
+f = open('mid-level-wp.json', 'r')
 wps = json.loads(f.read())['wikiprojects']
 f.close()
 
@@ -37,11 +35,14 @@ results = []
 skipped = []
 for wp in wps:
     wikiprojects = wps[wp]
-    w_processed = list(map(lambda k: "'{}'".format(k[10:].replace(' ', '_')), wikiprojects))
+    w_processed = list(map(lambda k: "'{}'".format(k[10:].replace(' ', '_')),
+                           wikiprojects))
     query = """SELECT tl_from, tl_title, tl_namespace, page.page_title
-        FROM templatelinks INNER JOIN page ON templatelinks.tl_from=page.page_id
+        FROM templatelinks INNER JOIN page
+        ON templatelinks.tl_from=page.page_id
         WHERE tl_title IN ( {} )
-        AND tl_namespace = 10 AND page.page_namespace = 1 AND page.page_is_redirect = 0
+        AND tl_namespace = 10 AND page.page_namespace = 1
+        AND page.page_is_redirect = 0
         ORDER BY RAND() LIMIT 2000""".format(','.join(w_processed))
     print("processing {}".format(wp))
     try:
@@ -51,16 +52,18 @@ for wp in wps:
             result = cur.fetchall()
             with open('wp-pairs', 'a') as fin:
                 for tup in result:
-                    new_tup = (tup[0], tup[1].decode('utf-8'), tup[2], tup[3].decode('utf-8'))
+                    new_tup = (tup[0], tup[1].decode('utf-8'),
+                               tup[2], tup[3].decode('utf-8'))
                     results.append((*new_tup, wp))
     except:
         skipped.append(wp)
         print("skipping {}".format(wp))
 
 df = pd.DataFrame(results)
-df.to_csv('wp-pairs', sep='\t', header=['page_id', 'wikiproject', 'namespace', 'page_title', 'category'])
+df.to_csv('wp-pairs', sep='\t', header=['talk_page_id', 'wikiproject',
+                                        'namespace', 'talk_page_title',
+                                        'category'])
 
-f = open('skipped','w')
+f = open('skipped', 'w')
 f.write(json.dumps(skipped))
 f.close()
-
