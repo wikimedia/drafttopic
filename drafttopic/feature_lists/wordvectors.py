5b9df5a1ccf9d55b2fa5bc5d4aee29db8b901809
Sumit Asthana <asthana.sumit23@gmail.com>
2018-03-13 23:14:12 +0530

    Add word vectors to drafttopic

---
+++ drafttopic/feature_lists/wordvectors.py
@@ -0,0 +1,20 @@
+from revscoring.datasources.meta import vectorizers
+from revscoring.features.meta import aggregators
+from revscoring.languages import english
+
+
+google_news_kvs = vectorizers.word2vec.load_kv(
+    path="~/ai/GoogleNews-vectors-negative300.bin", limit=150000)
+
+revision_text_vectors = vectorizers.word2vec(
+    english.stopwords.revision.datasources.non_stopwords,
+    google_news_kvs,
+    name="revision.text.google_news_vectors")
+
+w2v = aggregators.mean(
+    revision_text_vectors,
+    vector=True,
+    name="revision.text.google_news_vector_mean"
+)
+
+drafttopic = [w2v]
