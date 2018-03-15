aec5d16b5b1967c1b5aa037b0951465c65f07f6b
Sumit Asthana <asthana.sumit23@gmail.com>
2018-03-15 23:23:42 +0530

    Change word vector features to use a wrapper

--- drafttopic/feature_lists/wordvectors.py
+++ drafttopic/feature_lists/wordvectors.py
@@ -6,9 +6,12 @@ from revscoring.languages import english
 google_news_kvs = vectorizers.word2vec.load_kv(
     filename="GoogleNews-vectors-negative300.bin", limit=150000)
 
+def vectorize_words(words):
+    return vectorizers.word2vec.vectorize_words(google_news_kvs, words)
+
 revision_text_vectors = vectorizers.word2vec(
     english.stopwords.revision.datasources.non_stopwords,
-    google_news_kvs,
+    vectorize_words,
     name="revision.text.google_news_vectors")
 
 w2v = aggregators.mean(
