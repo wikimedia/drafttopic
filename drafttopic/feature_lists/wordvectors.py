1bffb81bd2e41fa4354f70c420d11c1eeacec283
Sumit Asthana <asthana.sumit23@gmail.com>
2018-03-15 23:27:46 +0530

    fix flake8

--- drafttopic/feature_lists/wordvectors.py
+++ drafttopic/feature_lists/wordvectors.py
@@ -6,8 +6,10 @@ from revscoring.languages import english
 google_news_kvs = vectorizers.word2vec.load_kv(
     filename="GoogleNews-vectors-negative300.bin", limit=150000)
 
+
 def vectorize_words(words):
     return vectorizers.word2vec.vectorize_words(google_news_kvs, words)
+
 
 revision_text_vectors = vectorizers.word2vec(
     english.stopwords.revision.datasources.non_stopwords,
