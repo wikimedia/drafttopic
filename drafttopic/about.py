cacff9789813f4f494d063b544ce9235926c13ff
Andy Craze <acraze@wikimedia.org>
2019-08-13 11:27:14 -0700

    fix authors string issue

--- drafttopic/about.py
+++ drafttopic/about.py
@@ -1,7 +1,7 @@
 __name__ = "drafttopic"
 __version__ = "0.1.1"
-__author__ = ["Aaron Halfaker", "Sumit Asthana"]
-__author_email__ = ["ahalfaker@wikimedia.org", "asthana.sumit23@gmail.com"]
+__author__ = "Aaron Halfaker, Sumit Asthana"
+__author_email__ = "ahalfaker@wikimedia.org, asthana.sumit23@gmail.com"
 __description__ = "A library for automatic detection of topics of new " +\
     "drafts on Wikipedia based on WikiProjects."
 __url__ = "https://github.com/wikimedia/drafttopic"
